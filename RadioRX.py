100e0772ec3d61a65e5cc51e9fab81638e099dff
MarcusDechant <marcusdechant@gmail.com>
2022-07-23 14:22:50 -0600

    Remote Sensor Script RX - v3.0.1 - Added SQLite3 Database

--- RadioRX.py
+++ RadioRX.py
@@ -1,4 +1,17 @@
-#!/bin/python3/Test
+#!/bin/python3/Radio
+
+#RadioProject
+#Remote Sensor
+#Marcus Dechant (c)
+#RadioRX.py
+#v3.0.1
+
+#verbose
+script = 'RadioRX.py'
+v = 'v3.0.1'
+author = 'Marcus Dechant (c)'
+verbose =('\n'+script+' - ('+v+') - '+author+'\n')
+print(verbose)
 
 import board as BO
 import busio as BU
@@ -22,6 +35,38 @@ EID2 = 0
 LID = 0
 c = ','
 
+cnct = sql.connect
+database = ('radio.db')
+db = cnct(database)
+xcte = db.execute
+save = db.commit
+clse = db.close
+
+xcte('''
+CREATE TABLE IF NOT EXISTS RADIO (
+LID     INT     NOT NULL    PRIMARY KEY,
+RLID    INT     NOT NULL,
+DELAY   INT     NULL,
+CODE    TEXT    NOT NULL,
+TEMP    REAL    NULL,
+HUMI    REAL    NULL,
+BTEMP   REAL    NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL);''')
+
+xcte('''
+CREATE TABLE IF NOT EXISTS ERROR (
+LID     INT     NOT NULL    PRIMARY KEY,
+EID     INT     NOT NULL,
+CODE    TEXT    NOT NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL);''')
+
+if path(database):
+    last = xcte('''SELECT LID FROM RADIO''')
+    for row in last:
+        LID = int(row[0])
+
 logs = (r'RadioLogs')
 if not path(logs):
     mkDir(logs)
@@ -35,40 +80,61 @@ with open(r'RadioLogs/radioTest.%s.csv' %LogID, 'w') as log:
 
 try:
     while True:
+        LID += 1
         packet = radio.receive(timeout=90)
         RSSI = radio.rssi
         SNR = radio.snr
-        rxData = (str(RSSI) + ',' + str(SNR))
+        rxData = (str(RSSI) +c+ str(SNR))
         try:
             data = str(packet, 'ascii')
+            #data = LID,RLID/EID,Delay,Code,Temp,Humi,bTemp,RSSI,SNR
             part = data.split(',')
-            ID = int(part[0])
-            DLY = int(part[1])
-            CODE = str(part[2])
-            Temp = float(part[3])
-            Humi = float(part[4])
+            loop = int(part[0])
+            delay = int(part[1])
+            code = str(part[2])
+            temp = float(part[3])
+            humi = float(part[4])
             bTemp = float(part[5])
+            
+            data = (str(LID)+c+data+c+rxData)
+            
+            xcte('''
+            INSERT INTO RADIO (LID,RLID,DELAY,CODE,TEMP,HUMI,BTEMP,RSSI,SNR)
+                       VALUES (?,?,?,?,?,?,?,?,?)''',
+                              (LID,loop,delay,code,temp,humi,bTemp,RSSI,SNR))
             
         except ValueError:
             print('\n' + str(packet))
             EID1 += 1
-            data = ('!!!,Err1,' + str(EID1) + ',,,')
-            #radio.reset()
+            data = (str(LID)+c+str(EID1)+',,Err1,,,,'+rxData)
+            ERR1 = 'Err1'
             
+            xcte('''
+            INSERT INTO ERROR (LID,EID,CODE,RSSI,SNR)
+                       VALUES (?,?,?,?,?)''',
+                              (LID,EID1,ERR1,RSSI,SNR))
+            
+            radio.reset()
+            radio = rfm9x(SPI, CS, RST, RF)
+                        
         except TypeError:
             print('\n' + str(packet))
             EID2 += 1
-            data = ('!!!,Err2,' + str(EID2) + ',,,')
-            #radio.reset()
-
-        LID += 1
-        data = (str(LID) + c + data + c + rxData)
+            data = (str(LID)+c+str(EID2)+',,Err2,,,,'+rxData)
+            ERR2 = 'Err2'            
+            
+            xcte('''
+            INSERT INTO ERROR (LID,EID,CODE,RSSI,SNR)
+                       VALUES (?,?,?,?,?)''',
+                              (LID,EID2,ERR2,RSSI,SNR))
+            
+            radio.reset()
+            radio = rfm9x(SPI, CS, RST, RF)
+        save()
         print('\n' + data)
-        
         with open(r'RadioLogs/radioTest.%s.csv' %LogID, 'a') as log:
             log.write(data + '\n')
-        radio.reset()
-        radio = rfm9x(SPI, CS, RST, RF)
         
 except KeyboardInterrupt:
+    clse()
     exit(0)
