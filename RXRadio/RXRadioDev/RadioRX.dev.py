28716df4bf7f019fb68ec27d21867d7ccbcb8680
MarcusDechant <marcusdechant@gmail.com>
2022-07-23 23:53:22 -0600

    Radio Dev Files

---
+++ RXRadio/RXRadioDev/RadioRX.dev.py
@@ -0,0 +1,194 @@
+#!/bin/python3/Radio/dev
+
+#RadioProject
+#Remote Sensor
+#Marcus Dechant (c)
+#RadioRX.py
+#v3.0.4
+
+#verbose
+script = 'RadioRX.py'
+v = 'v3.0.4'
+author = 'Marcus Dechant (c)'
+verbose =('\n'+script+' - ('+v+') - '+author+'\n')
+print(verbose)
+
+#Import List
+import board as BO
+import busio as BU
+import digitalio as DIO
+import adafruit_rfm9x as RFM
+import os
+import sqlite3 as sql
+from datetime import datetime
+path = os.path.exists
+mkDir = os.mkdir
+digIO = DIO.DigitalInOut
+
+#Radio Object (SPI)
+rfm9x = RFM.RFM9x
+SPI = BU.SPI(BO.SCK, MOSI=BO.MOSI, MISO=BO.MISO)
+CS = digIO(BO.CE1)
+RST = digIO(BO.D25)
+RF = 915.0
+radio = rfm9x(SPI, CS, RST, RF)
+
+#local variables
+EID1 = 0
+EID2 = 0
+LID = 0
+
+#comma seperator
+c = ','
+
+#database
+cnct = sql.connect
+database = ('radio.db')
+db = cnct(database)
+
+#database constructors
+xcte = db.execute
+save = db.commit
+clse = db.close
+
+#reading database
+#LID = Loop ID
+#RLID = Remote Loop ID
+#TYME = time of reading
+#DELAY = time between readings
+#CODE = status descriptor
+#TEMP = SHT30 Temperature Reading
+#HUMI = SHT30 Relative Humidity Reading
+#BTEMP = Raspberry Pi Pico Board Temperature
+#RSSI = RFM95W Return Signal Strenght Indication
+#SNR = RFM95W Signal to Noise Ratio
+#DATE = date of reading
+xcte('''
+CREATE TABLE IF NOT EXISTS RADIO (
+LID     INT     NOT NULL    PRIMARY KEY,
+RLID    INT     NOT NULL,
+TYME    TEXT    NOT NULL,
+DELAY   INT     NULL,
+CODE    TEXT    NOT NULL,
+TEMP    REAL    NULL,
+HUMI    REAL    NULL,
+BTEMP   REAL    NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL,
+DATE    TEXT    NOT NULL);''')
+
+#error database
+xcte('''
+CREATE TABLE IF NOT EXISTS ERROR (
+LID     INT     NOT NULL    PRIMARY KEY,
+EID     INT     NOT NULL,
+TYME    TEXT    NOT NULL,
+CODE    TEXT    NOT NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL,
+DATE    TEXT    NOT NULL);''')
+
+"""
+#error databases
+#EXPERIMENTAL
+xcte('''
+CREATE TABLE IF NOT EXISTS ERR1 (
+LID     INT     NOT NULL    PRIMARY KEY,
+TIME    TEXT    NOT NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL,
+DATE    TEXT    NOT NULL);''')
+
+xcte('''
+CREATE TABLE IF NOT EXISTS ERR2 (
+LID     INT     NOT NULL    PRIMARY KEY,
+TIME    TEXT    NOT NULL,
+RSSI    REAL    NOT NULL,
+SNR     REAL    NOT NULL,
+DATE    TEXT    NOT NULL);''')
+"""
+
+#database continuation
+#gets last LID from 
+if path(database):
+    last = xcte('''SELECT LID FROM RADIO''')
+    for row in last:
+        LID = int(row[0])
+
+logs = (r'RadioLogs')
+if not path(logs):
+    mkDir(logs)
+    
+LogID = 0
+while path (r'RadioLogs/radioTest.%s.csv' %LogID):
+    LogID += 1
+    
+with open(r'RadioLogs/radioTest.%s.csv' %LogID, 'w') as log:
+    log.write('LID, ID, Delay, Code, TempC, Humi, BoTempC, RSSI, SNR, TIME, DATE\n')
+
+print('LID, ID, Delay, Code, TempC, Humi, BoTempC, RSSI, SNR, TIME, DATE\n')
+try:
+    while True:
+        LID += 1
+        packet = radio.receive(timeout=90)
+        rssi = radio.rssi
+        snr = radio.snr
+        date = datetime.now().strftime('%d/%m/%Y')
+        tyme = datetime.now().strftime('%H:%M:%S')
+        datetyme = (tyme+c+date)
+        rxData = (str(rssi) +c+ str(snr))
+        
+        try:
+            data = str(packet, 'ascii')
+            #data = LID,RLID/EID,Delay,Code,Temp,Humi,bTemp,RSSI,SNR
+            part = data.split(',')
+            loop = int(part[0])
+            delay = int(part[1])
+            code = str(part[2])
+            temp = float(part[3])
+            humi = float(part[4])
+            bTemp = float(part[5])
+            
+            data = (str(LID)+c+data+c+rxData+c+datetyme)
+            
+            xcte('''
+            INSERT INTO RADIO (LID,RLID,TYME,DELAY,CODE,TEMP,HUMI,BTEMP,RSSI,SNR,DATE)
+                       VALUES (?,?,?,?,?,?,?,?,?,?,?)''',
+                              (LID,loop,tyme,delay,code,temp,humi,bTemp,rssi,snr,date))
+            
+        except ValueError:
+            #print(str(packet)+'\n')
+            EID1 += 1
+            ERR1 = 'Err1'
+            data = (str(LID)+c+str(EID1)+c+c+ERR1+c+c+c+c+rxData+c+datetyme)
+                        
+            xcte('''
+            INSERT INTO ERROR (LID,EID,TYME,CODE,RSSI,SNR,DATE)
+                       VALUES (?,?,?,?,?,?,?)''',
+                              (LID,EID1,tyme,ERR1,rssi,snr,date))
+            
+            radio.reset()
+            radio = rfm9x(SPI, CS, RST, RF)
+                        
+        except TypeError:
+            #print(str(packet)+'\n')
+            EID2 += 1
+            ERR1 = 'Err2'
+            data = (str(LID)+c+str(EID2)+c+c+ERR2+c+c+c+c+rxData+c+datetyme)            
+            
+            xcte('''
+            INSERT INTO ERROR (LID,EID,TYME,CODE,RSSI,SNR,DATE)
+                       VALUES (?,?,?,?,?,?,?)''',
+                              (LID,EID2,tyme,ERR2,rssi,snr,date))
+            
+            radio.reset()
+            radio = rfm9x(SPI, CS, RST, RF)
+        save()
+        
+        print(data+'\n')
+        with open(r'RadioLogs/radioTest.%s.csv' %LogID, 'a') as log:
+            log.write(data + '\n')
+        
+except KeyboardInterrupt:
+    clse()
+    exit(0)
---
+++ RXRadio/RXRadioDev/dbQ.rx.dev.py
@@ -0,0 +1,77 @@
+#!/bin/python3/Radio/dev
+
+#SHT Project
+#Homebrew SCADA
+#Marcus Dechant (c)
+#dbQ.rx.py
+#v0.1.2
+
+#verbose
+script = 'dbQ.rx.py'
+v = 'v0.1.2'
+author = 'Marcus Dechant (c)'
+verbose =('\n'+script+' - ('+v+') - '+author+'\n')
+print(verbose)
+
+#Import List
+import sqlite3 as sql
+
+#comma separator
+c = (', ')
+
+#user chooses database
+dbNum = input('Radio (1) or Error (2)?\n')
+
+#database
+conn = sql.connect
+database = ('radio.db')
+db = conn(database)
+
+#database constructors
+xcte = db.execute
+save = db.commit
+clse = db.close
+
+#RX database
+if (dbNum == '1') or (dbNum == 'Radio'): 
+    #query all from database
+    cur = xcte('''SELECT * FROM RADIO''')
+    #database header
+    print('\nLID, RLID, TYME, DELAY, CODE, TEMP, HUMI, bTEMP, RSSI, SNR, DATE')
+    #select contents from database
+    for row in cur:
+        r0 = str(row[0])
+        r1 = str(row[1])
+        r2 = str(row[2])
+        r3 = str(row[3])
+        r4 = str(row[4])
+        r5 = str(row[5])
+        r6 = str(row[6])
+        r7 = str(row[7])
+        r8 = str(row[8])
+        r9 = str(row[9])
+        r10 = str(row[10])
+        #displays contents of database
+        print(r0+c+r1+c+r2+c+r3+c+r4+c+r5+c+r6+c+r7+c+r8+c+r9+c+r10)
+
+#error database
+elif (dbNum == '2') or (dbNum == 'Error'):
+    cur = xcte('''SELECT * FROM ERROR''')
+    print('\nLID, EID, TYME, CODE, RSSI, SNR, DATE')
+    for row in cur:
+        r0 = str(row[0])
+        r1 = str(row[1])
+        r2 = str(row[2])
+        r3 = str(row[3])
+        r4 = str(row[4])
+        r5 = str(row[5])
+        r6 = str(row[6])
+        print(r0+c+r1+c+r2+c+r3+c+r4+c+r5+c+r6)
+
+#user selection error
+else:
+    print('\nError. Not an option.')
+
+print('\nEnd of Database')
+clse()
+exit(0)
