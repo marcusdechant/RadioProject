a6a2d10cdf73479c8d883b1c8bdb78a3c4e7393c
MarcusDechant <marcusdechant@gmail.com>
2022-08-14 09:33:15 -0600

    Minor Change

--- TXRadio/sht.py
+++ TXRadio/sht.py
@@ -1,9 +1,10 @@
 #sht.py
+#(c) Marcus Dechant 2022
 #Temperature and Humidity Sensor
 import lib.SHT31D.adafruit_sht31d as SHT
 import busio as BU
 import board as BO
-ICC=BU.I2C(scl=BO.GP19, sda=BO.GP18, frequency=200000)
+ICC=BU.I2C(scl=BO.GP19,sda=BO.GP18,frequency=200000)
 sht30=SHT.SHT31D(ICC)
 def t():
     TEMP=sht30.temperature
@@ -13,4 +14,4 @@ def h():
     HUMI=sht30.relative_humidity
     humi=('{0:0.2f}'.format(HUMI))
     return(humi)
-#(c) Marcus Dechant 2022
+
